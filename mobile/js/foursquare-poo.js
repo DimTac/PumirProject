4e303dac1d4df4c756096dd7e5705a130b817599
Lucie Reitz <lucie.reitz@gmail.com>
2013-11-22 20:28:18 +0100

    Fixs + ménage

---
+++ mobile/js/foursquare-poo.js
@@ -0,0 +1,280 @@
+/**
+ * @date: 04/11/2013
+ * @auteur: P. GUILHOU - HETIC P2016
+ * @version: 1.4
+ * @project: WheeLunch
+ * 
+ * Tutos utilisés :
+ * 	  https://developer.foursquare.com/overview/tutorial
+ * 	  https://developer.foursquare.com/overview/auth#code
+ * 	  https://developer.foursquare.com/docs/users/friends
+ * 	  https://developer.foursquare.com/docs/checkins/recent
+ * URL appli perso (données appli):
+ * 	  https://fr.foursquare.com/developers/app/1LZQR0XMLY0L0XQ5JA44HQ2QTG15GHBKUQ4CE5K2TOA1UH0Z
+ *
+ * Identifiants foursquare de l'application :
+ *    Client_id : 1LZQR0XMLY0L0XQ5JA44HQ2QTG15GHBKUQ4CE5K2TOA1UH0Z
+ *    Secret    : YNEV10LLQZT1VOHLTFWAYWZQ5WREYX31ATVS0THGOU34CNR4    
+*/
+
+	var Foursquare = {
+
+		params : {
+			client_id   : '',
+			secret      : '',
+			token       : '',
+			position    : '',
+			url         : '',
+			search      : '',
+			version     : '',
+			callback_oauth   : function(){},
+			callback_request : function(){}
+		},
+
+		/**
+		 * Initialise tous les paramètres de l'objet
+		 * @param  {Object} options La liste des paramètres envoyés
+		 * @return {rien}         Ne retourne rien
+		 */
+		init : function(options){
+			this.property = $.extend(this.params, options);
+		},
+
+		/**
+		 * Initialise les coordonnées GPS de l'utilisateur
+		 * s'il accepte la géolocalisation
+		 * @return {String} Coordonnées GPS formatées
+		 */
+		goGetLocation : function(){
+			var that = this;	// Pour les fonctions de callback
+			if(navigator.geolocation){
+				navigator.geolocation.getCurrentPosition(
+					function(pos){
+						that.property.position = pos.coords.latitude+","+pos.coords.longitude;
+						that.property.callback_geolocation.call(that, that);
+					}, 
+					function(error){
+						switch(error.code){
+					      case error.PERMISSION_DENIED:
+					        alert("Vous avez refuse l\'acces a la geolocalisation.");
+					        break;
+					      case error.POSITION_UNAVAILABLE:
+					        alert("Les informations de geolocalisation sont indisponibles.");
+					        break;
+					      case error.TIMEOUT:
+					        alert("Le temps de reponse de la requete est trop long - arret.");
+					        break;
+					      case error.UNKNOWN_ERROR:
+					      default:
+					        alert("Une erreur inconnue s\'est produite.");
+					        break;
+					    }
+						that.property.position = '48.859068,2.352249';	// Paris, par défaut
+					},
+					{enableHighAccuracy : true}
+				);
+			}
+		},
+
+		/**
+		 * Lance l'authentification OAuth afin de récupérer
+		 * des informations personnelles Foursquare relatives
+		 * à l'utilisateur et sa communauté sur le site.
+		 * @return {rien} Ne retourne rien
+		 */
+		launchOAuth : function(){
+			console.log('Oauth lancé');
+			this.property.callback_oauth.call(this, this);
+		},
+
+		/**
+		 * Renvoie l'attribut position
+		 * @return {String} La position GPS
+		 */
+		getLocation : function(){
+			return this.property.position;
+		},
+
+		/**
+		 * Initialise la position géographique
+		 * de l'utilisateur avec l'objet userLocation obtenu
+		 * grace à la géolocalisation
+		 * @param  {Object} userLocation L'objet contenant la géoposition
+		 * @return {rien}              Pas de retour
+		 */
+		setPosition : function(userLocation){
+			if(typeof userLocation === 'object')	// Si l'utilisateur a accepté la géolocalisation
+				this.property.position = userLocation.latitude+','+userLocation.longitude;
+		},
+
+		/**
+		 * Initialise la date / version demandée par Foursquare
+		 * @return {rien} Ne renvoit rien
+		 */
+		generateVersion : function(){
+			var d     = new Date();
+			var year  = d.getFullYear();
+			var month = d.getMonth()+1;
+			var day   = d.getDate();
+
+            if(month <10)
+                month = "0"+month;
+            if(day <10)
+                day = "0"+day;
+            this.property.version = year+''+month+''+day;
+		},
+
+		/**
+		 * Fonction qui envoie la requete ajax à foursquare après
+		 * avoir récupéré les coordonnées GPS de l'utilisateur,
+		 * afin de cibler les évènements/avis de ses amis à proximité
+		 * 
+		 * @return {JSON} Le json renvoyé par foursquare
+		 */
+		envoi_requete_foursquare : function(){
+			var that = this;
+			$.ajax({
+					url: this.property.url,
+					dataType: "json",
+				    success: function(json) {
+				    	that.property.callback_request.call(this, that, json);	// On appelle la fonction utilisateur
+				    	return;
+					},
+					error: function(){
+						console.log('Erreur dans la requete ajax Fourquare')
+						alert('La requete Foursquare a echoue...');
+						return null;
+					}
+			});
+		},
+
+		/**
+		 * Construit l'URL qui va chercher les informations
+		 * sur la plateforme Foursquare selon le token
+		 * et d'autres paramètres variables
+		 * @return {rien} Ne retourne rien
+		 */
+		setURL_Foursquare : function(){
+			var obj = this.property;
+			//obj.url = "https://api.foursquare.com/v2/users/self/friends?oauth_token="+obj.token+"&v="+obj.version;
+			//obj.url = "https://api.foursquare.com/v2/venues/earch?ll="+obj.position+"&query=kebab&oauth_token="+obj.token+"&v="+obj.version;
+			obj.url = "https://api.foursquare.com/v2/venues/explore?ll="+obj.position+"&oauth_token="+obj.token+"&section=food&limit=10&friendVisits=visited"+"&v="+obj.version;
+		},
+
+		/**
+		 * Permet d'initialiser une requête de phase 
+		 * de test ( version simplifiée sans paramètres )
+		 * @param  {String} url L'url voulue
+		 * @return {rien}     Ne retourne rien (setter)
+		 */
+		setURL_Foursquare_perso : function(url){
+			this.property.url = url;
+		},
+
+		/**
+		 * Getter associé au token Foursquare
+		 * présent dans l'url
+		 * @return {String} Token
+		 */
+		getToken : function(){
+			return this.property.token;
+		},
+
+		/**
+		 * Getter associé au token Foursquare
+		 * présent dans l'url
+		 * @return {String} Token
+		 */
+		getTokenURL : function(){
+			var parser = document.createElement('a');
+			parser.href = document.location.href;
+			return parser.hash.substr(14);
+		},
+
+		/**
+		 * Récupère le token dans l'url renvoyé par Foursquare,
+		 * puis initialise le token de l'objet
+		 * @return {rien} Ne retourne rien
+		 */
+		setToken : function(){
+			var parser = document.createElement('a');
+			parser.href = document.location.href;
+			this.property.token = parser.hash.substr(14);
+		},
+
+		/**
+		 * Méthode OAuth 2 :
+		 * Va chercher le token sur la plateforme Foursquare
+		 * en demandant à l'utilisteur de s'identifier.
+		 * @return {[type]} Ne retourne rien; une fois l'authentification
+		 *                  effectuée, l'utilisateur est renvoyé sur le site;
+		 */
+		goGetToken : function(){
+			window.location.replace("https://foursquare.com/oauth2/authenticate?client_id="+this.property.client_id+"&response_type=token&redirect_uri=http://wheelunch.fr/panel-roue.html");
+			// &display=webpopup
+		},
+
+		/**
+		 * Renvoie l'url de feed foursquare de l'objet
+		 * @return {String} L'url
+		 */
+		getURL : function(){
+			return this.property.url;
+		},
+
+		/**
+		 * Initialise l'URL de feed voulue,
+		 * selon le type de données à aller chercher
+		 * @param  {String} url L'url
+		 * @return {rien}     Ne retourne rien
+		 */
+		setURL : function(url){
+			this.property.url = url;
+		},
+
+		/**
+		 * Initialise le mot clef de feed voulu,
+		 * par exemple un nom de restaurant, une catégorie etc...
+		 * @param  {String} search Le mot-clef demandé
+		 * @return {rien}     Ne retourne rien
+		 */
+		setSearch : function(search){
+			this.search = search;
+		},
+
+		/**
+		 * Initialise le code client de l'application
+		 * @param  {String} id L'id du client
+		 * @return {rien}     Ne retourne rien
+		 */
+		setClientId : function(id){
+			this.property.client_id = id;
+		},
+
+		/**
+		 * Initialise le code secret de l'application
+		 * @param  {String} secret Le secret du client
+		 * @return {rien}     Ne retourne rien
+		 */
+		setSecret : function(secret){
+			this.property.secret = secret;
+		},
+
+		/**
+		 * Retourne la version courante de l'objet
+		 * si elle existe
+		 * @return {String} La version générée
+		 */
+		getVersion : function(){
+			return this.property.version;
+		},
+
+		/**
+		 * Fonction qui détruit l'objet et ses paramètres
+		 * @return {rien} Ne renvoit rien
+		 */
+		finalize : function(){
+			this.property = $.extend(this.params, null);	
+		}
+
+	};
---
+++ mobile/js/roueTourne.js
@@ -0,0 +1,278 @@
+/*  Code inspired (and translated) from a JQuery Plug-in :
+ *  @author Roy Yu | iroy2000 [at] gmail.com 
+ *  @description: This jquery plugin will create a tourne wheel and let you to add players at run time. 
+ *  http://iroylabs.blogspot.com/2011/06/latest-jquery-plugin.html
+ 
+ *   USES JQUERY OBJECT + CANVAS DRAWING :) 
+ */
+
+
+(function($){
+    $.fn.rouetourne = function(options, callback){
+        
+        //////////// INITIALISATION VARIABLES + PARAMETRES PAR DEFAUT DE L'OBJET ////////////
+        var paramsRoue = $.extend({},$.fn.rouetourne.default_options, options), 
+        $that = $(this), 
+        ctx = null,
+        startAngle = 90, 
+        arc = Math.PI / 5, 
+        tourneTimeout = null, 
+        tourneArcStart = 10, 
+        tourneTime = 0, 
+        modifResto = true,
+        tourneTimeTotal = 0, 
+        tourneAngleStart = 0, 
+        restoLength = 10, 
+        resultatRoue = null,
+        restoArray = paramsRoue.restoArray;
+
+        if($.isFunction(options)){
+            callback = options;
+            options = {};
+        } 
+        
+         //////////// DECLARATION METHODES DE L'OBJET ////////////
+        var methods = {
+            init: function() {
+                methods.getContext();
+                methods.setup();
+                drawroue();                
+            },
+
+            /////////////// SET-UP DES EVENEMENTS QUI DECLENCHERONT LES METHODES //////////////// 
+            setup: function() {
+                //Un click sur "tourneDeclencheur" (voir dans les options) fera tourner la roue
+                $(paramsRoue.tourneDeclencheur).bind('click', function(e){
+                    e.preventDefault();
+                    methods.tourne();
+                });
+                
+                //Un click sur les types de resto ajoutera ou supprimera des quartiers                             
+                $(paramsRoue.typeRestoDeclencheur).bind('click', function(e){
+                    e.preventDefault();
+                    methods.updatePanel(e);
+                });
+                
+                //Un click sur le bouton back lancera la fonction back
+                $(paramsRoue.backDeclencheur).bind('click', function(e){
+                    e.preventDefault();
+                    methods.back();
+                });                
+            },
+
+            /////////////// RECUPERATION DU CONTEXTE POUR DRAW DANS LE CANVAS ////////////////            
+            getContext: function() {  
+                if(ctx !== null)
+                    return ctx;
+
+                 var canvas = document.getElementById("canvas");
+                 var G_vmlCanvasManager;
+
+                if (G_vmlCanvasManager != undefined) { // if IE9
+                        G_vmlCanvasManager.initElement(canvas);
+                }
+
+                if (canvas.getContext) {
+                        ctx = canvas.getContext('2d');
+                }   
+            },
+
+            /////////////// QUAND ON CLIQUE SUR BACK ////////////////
+            back : function(){
+                // $("#affichage").animate(
+                //     {"margin-left":"80%"},
+                //     {
+                //       duration:500,
+                //     }).animate(
+                //     {"width":"20%"},
+                //     {
+                //       duration:500,
+                //     });
+                //   $("#panel").css('width', '100%');
+                //   $("#map").css('width', '0%');
+                //   $("#panel-results").fadeOut(500);
+                //   $("#panel-roue").fadeIn(500);
+                $("#panel").delay(1000).animate(
+                    {"margin-left":"75%"},
+                    {duration:500}
+                );
+                $("#panel-results").delay(1500).fadeOut(500);
+                $("#panel-roue").delay(1500).fadeIn(500);
+                $('#map').fadeOut(1000);
+                  $(paramsRoue.restoResultatDiv+">img").fadeIn(1200);
+                  $("#canvas").fadeIn(1000);
+                  $("#fleche").fadeIn(750);
+
+                  modifResto=true;
+            },
+            //Fonction qui fait tourner la roue !
+            tourne: function() {
+                tourneAngleStart = Math.random() * 10 + 10;
+                tourneTime = 0;
+                tourneTimeTotal = Math.random() * 3 + 4 * 1000;
+                rotateroue();                
+            },
+            //Fonction qui met à jour la roue quand on clique sur les types de resto : ajout ou suppression de quartiers
+            updatePanel: function(evenement) { 
+                if (modifResto){
+                //Si la roue n'est pas en train de tourner :
+                    var trouve=false;
+
+                    var typeRestoEnCours = evenement.currentTarget;
+                    $restoAjout = typeRestoEnCours.value;
+                    var index = $.inArray($restoAjout, restoArray);
+                    if (index>-1){
+                        trouve=true;
+                    }
+
+                    //Si le type de resto n'est pas dans le tableau, on l'ajoute, sauf si le tableau est déjà plein
+                    if ((!trouve)&&(restoArray.length<10)){
+                        $(typeRestoEnCours).toggleClass('selected');
+                        restoArray.push($restoAjout);
+                        restoLength = $restoAjout.length;
+                        arc = Math.PI / (restoArray.length/2);
+                        drawroue();
+                    }
+                    
+                    //Sinon on le supprime (sauf si le tableau contient moins de deux éléments, auquel cas on n'enlève pas les deux derniers éléments)
+                    else if ((trouve)&&(restoArray.length>2)){
+                        $(typeRestoEnCours).toggleClass('selected');
+                        restoArray.splice(index, 1);
+                        restoLength = restoArray.length;
+                        arc = Math.PI / (restoArray.length/2);
+                        drawroue();
+                    }
+                }
+            }
+        }
+        
+       var rotateroue = function rotateroue() {
+                tourneTime += 30;
+                if(tourneTime >= tourneTimeTotal) {
+                    stopRotationRoue();
+                    return;
+                }
+                modifResto=false;
+                var tourneAngle = tourneAngleStart - easeOut(tourneTime, 0, tourneAngleStart, tourneTimeTotal);
+                startAngle += (tourneAngle * Math.PI / 180);
+                drawroue();
+                tourneTimeout = setTimeout(rotateroue, 30);
+        }
+        
+        /////////// QUAND LA ROUE A FINI DE TOURNER ////////////////////  
+        function stopRotationRoue () {
+                clearTimeout(tourneTimeout);
+                //Quel type de resto va gagner ?
+                var degrees = startAngle * 180 / Math.PI + 90;
+                var arcd = arc * 180 / Math.PI;
+                var index = Math.floor((360 - degrees % 360) / arcd);
+                ctx.save();
+
+                ctx.font = paramsRoue.resultTextFont;
+                resultatRoue = restoArray[index];
+
+                //Affichage, à la place du logo, du type de resto gagnant :
+                 $(paramsRoue.restoResultatDiv).children().fadeOut(0, function() {
+                    $(paramsRoue.restoResultatDiv+">p").text(resultatRoue).fadeIn(100);
+                  });
+
+                 $(paramsRoue.restoResultatDiv+">p").fadeOut(0, function() {
+                    $.rotation_complete(resultatRoue);
+                    $("#canvas").fadeOut(500);
+                    $("#fleche").fadeOut(500);
+                    ctx.restore();
+                  });
+            }         
+/*        
+        /////////// DESSIN DE LA FLECHE ////////////////////  
+        function drawArrow() {
+            ctx.fillStyle = paramsRoue.arrowColor;
+            ctx.beginPath();
+            ctx.moveTo(0 - 4, 250 - (paramsRoue.outterRadius + 15));
+            ctx.lineTo(0 + 4, 250 - (paramsRoue.outterRadius + 15));
+            ctx.lineTo(0 + 4, 250 - (paramsRoue.outterRadius - 15));
+            ctx.lineTo(0 + 9, 250 - (paramsRoue.outterRadius - 15));
+            ctx.lineTo(0 + 0, 250 - (paramsRoue.outterRadius - 23));
+            ctx.lineTo(0 - 9, 250 - (paramsRoue.outterRadius - 15));
+            ctx.lineTo(0 - 4, 250 - (paramsRoue.outterRadius - 15));
+            ctx.lineTo(0 - 4, 250 - (paramsRoue.outterRadius + 15));
+            ctx.fill();               
+        }*/
+
+        /////////// DESSIN DE LA ROUE ////////////////////      
+        function drawroue() {
+            ctx.strokeStyle = paramsRoue.roueBorderColor;
+            ctx.lineWidth = paramsRoue.roueBorderWidth;
+            ctx.font = paramsRoue.roueTextFont;            
+            ctx.clearRect(0,0,500,500);
+            var text = null, 
+            i = 0,
+            totalJoiner = restoArray.length;
+
+            for(i = 0; i < totalJoiner; i++) {
+                text = restoArray[i];
+                //On met le texte/picto qui correspond au type de resto           
+                var angle = startAngle + i * arc; 
+                //console.log(restoArray[i]+ " angle : "+ angle + " arc : "+ arc+ " startangle "+ startAngle);      
+
+                /////////// DESSIN DE LA ROUE : COULEUR ////////////////////      
+
+              if (i%2){
+                //Quartiers paires d'une certaine couleur
+                     ctx.fillStyle="#bb5048";
+                }
+                else{
+                     ctx.fillStyle="#9c4b47";
+                }
+
+                ctx.beginPath();
+                // ** arc(centerX, centerY, radius, startingAngle, endingAngle, antiClockwise);
+                ctx.arc(0, 400, paramsRoue.outterRadius, angle, angle + arc, false);
+                ctx.arc(0, 400, paramsRoue.innerRadius, angle + arc, angle, true);
+                ctx.stroke();
+                ctx.fill();
+        
+                ctx.save();
+
+                /////////// TRAITEMENT DU TEXTE SUR LA ROUE ////////////////////
+
+                ctx.fillStyle     = paramsRoue.roueTextColor;
+                ctx.translate(0 + Math.cos(angle + arc / 2) * paramsRoue.textRadius, 400 + Math.sin(angle + arc / 2) * paramsRoue.textRadius);
+                ctx.rotate(angle + arc / 2 + Math.PI / 2);
+                
+                ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+                ctx.restore();
+                ctx.closePath();
+            }     /*
+                drawArrow();*/
+        }          
+
+        /////////// ANIMATION DE LA ROUE QUAND ELLE TOURNE ////////////////////  
+        function easeOut(t, b, c, d) {
+            var ts = (t/=d)*t;
+            var tc = ts*t;
+            return b+c*(tc + -3*ts + 3*t);
+        } 
+                
+        methods.init.apply(this,[]);
+    }
+    
+
+     //////////// OPTIONS DE L'UTILISATEUR (UI) DE L'OBJET ////////////
+    $.fn.rouetourne.default_options = {
+        outterRadius:400, 
+        innerRadius:150, 
+        textRadius: 300, 
+        tourneDeclencheur: '#go', 
+        backDeclencheur : '#back',
+        roueBorderColor: 'white',
+        roueBorderWidth : 30, 
+        roueTextFont : '22px sans-serif', 
+        roueTextColor: 'white', /*
+        roueTextShadowColor : 'none',*/
+        resultTextFont : '30px sans-serif', 
+        arrowColor :'black',
+        typeRestoDeclencheur:'#choix :input',
+        restoResultatDiv:'#restoDiv'
+    }
+})(jQuery);
