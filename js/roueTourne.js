04b7e890826724c0b21c8ee6887e8680c5ab2ceb
Jérémy Zaloga <jeremy.zaloga@gmail.com>
2013-11-23 00:53:02 +0100

    ceci n'est pas un commit

--- js/roueTourne.js
+++ js/roueTourne.js
@@ -190,7 +190,7 @@
                       chaine += '<img class="pictoResultat" src=img/'+resultatRoueSrc+' alt='+resultatRoue+'>';
                       chaine += '<h1>'+resultatRoue+'</h1>';
                       chaine += '<p>'+resultatRoueSlogan+'</p>';
-                      chaine += '<button id="relancer">Relancer<img src="img/reload.png" alt="next"></button><button id="bonchoix">Bon choix<img src="img/next.png" alt="next"></button>'
+                      chaine += '<button id="bonchoix">Bon choix<img src="img/next.png" alt="next"></button><button id="relancer">Relancer<img src="img/reload.png" alt="next"></button>';
                       chaine += '</div>';                     
                       $(paramsRoue.restoResultatDiv).append(chaine);
                       chaine = '';
@@ -216,11 +216,8 @@
         function drawArrow() {
             var posX = ($(canvas).width())-135;
             var posY = ($(canvas).height())-590;
-<<<<<<< HEAD
             // console.log(posY);
             // console.log(posX);
-=======
->>>>>>> dce00b2c63b309823819d5c0e02b291cbf9f5103
             var fleche=new Image();
             fleche.onload = function() {
                      ctx.drawImage(fleche, posX, posY);
