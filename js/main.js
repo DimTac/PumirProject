a31f91208fe0963c8c6af5e426e584b8bbac539c
DimTac <dimitri.tacita@gmail.com>
2013-11-23 19:51:01 +0100

    [FIX] Vidéos

--- js/main.js
+++ js/main.js
@@ -287,7 +287,10 @@ $(document).ready(function(){
 	              chaine += '<h4>Il va falloir cuisiner par vous-même cette <a href="'+json.url+'">recette de '+json.nom+'</a> !</h4>';
 	              chaine += '<div id="recette">';
 	                console.log(json.url_video);
-	                chaine += '<video style="margin:0 auto;" width="550" height="240" controls="controls"><source src="http://wheelunch.fr/_pierre/assets/'+json.url_video+'" type="video/mp4" />'+mot_clef+'</video>';
+	                chaine += '<video style="margin:0 auto;" width="550" height="240" controls="controls">';
+	                chaine += '<source src="assets/'+json.url_video+'" type="video/mp4" />';
+	                chaine += '<source src="assets/'+json.url_video_webm+'" type="video/webm" />';
+	                chaine += '<source src="assets/'+json.url_video_ogg+'" type="video/ogg" />'+mot_clef+'</video>';
 	               chaine += '</div>';
 	            chaine += '</div>';
 	            chaine += '<div class="modal-footer" style="background-color:#be4c46;">';
